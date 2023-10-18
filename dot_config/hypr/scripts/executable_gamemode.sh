2aeb26e81d02ff0ea53c5cd6d793cff243321e7c
Sakari <sk308269317@outlook.com>
2023-10-18 11:18:10 +0800

    hyprland updates

--- dot_config/hypr/scripts/executable_gamemode.sh
+++ dot_config/hypr/scripts/executable_gamemode.sh
@@ -5,6 +5,7 @@ if [ $HYPRGAMEMODE = 1 ]; then
         keyword animations:enabled 0;\
         keyword decoration:drop_shadow 0;\
         keyword decoration:blur:enabled 0;\
+        keyword decoration:inactive_opacity 1.0;\
         keyword general:gaps_in 0;\
         keyword general:gaps_out 0;\
         keyword general:border_size 1;\
